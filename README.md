69726bfd8ad8b73a3a8a21ac266672dcb3c8d612
LeninPA <lenin.pavon@comunidad.unam.mx>
2020-05-14 13:01:01 -0500

    FEAT: Creación de actividad2.php

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SW_2020PavonAlvarezLenin
+Lenin Pavón Alvarez
+Nunca en mi vida me había sentido tan frustrado. Mas el adquirir
+tal cantidad de conocimientos y habilidades en un corto tan
+pequeño periodo de tiempo he de admitir que es sencillamente
+impresionante. Aunque quisiera dormir más.
+¡Hola GitHub!
---
+++ actividad2.php
@@ -0,0 +1,15 @@
+<?php
+  echo "Mi nombre es: Lenin Pavón Alvarez";
+  echo "He aprendido:
+  <ul>
+    <li>Como funcionan las COOKIES y las sesiones</li>
+    <li>A perderle miedo a mi consola uwu</li>
+  </ul>
+  Me ha gustado:
+  <ul>
+    <li>Hacer el vieojuego, fue hermoso</li>
+    <li>Aprender a usar php</li>
+  </ul>
+
+  ";
+ ?>
